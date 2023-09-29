8340dc77ddea68ba1155e0ca758d48ec687032d6
harshit9286 <117741030+harshit9286@users.noreply.github.com>
2023-09-29 23:59:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Keyboard from './components/Keyboard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Keyboard/>
+    </div>
+  );
+}
+
+export default App;
